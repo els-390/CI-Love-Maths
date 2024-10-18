e5ae174ac41308f4c0e56396c159fdddaadad016
Ellie Carpenter <espanswick@gmail.com>
2024-10-18 10:53:28 +0000

    Add: Function for calculate correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,13 +35,29 @@ function runGame(gameType) {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
-   }
+
+}
 
 function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -63,10 +79,6 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "-";
-    
 }
 
 function displayMultiplyQuestion() {
