f5a8aab98be1077109099845ab107a7752be0b56
Ellie Carpenter <espanswick@gmail.com>
2024-10-18 16:00:32 +0000

    Add: Division code including allowing for decimal numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-document.getElementById("answer-box").addEventListener("keydown", function(event){
-  if (event.key === "Enter"){
-    checkAnswer();
-  }
-})
+  document
+    .getElementById("answer-box")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        checkAnswer();
+      }
+    });
   runGame("addition");
 });
 
@@ -27,7 +29,6 @@ document.getElementById("answer-box").addEventListener("keydown", function(event
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
-
   document.getElementById("answer-box").value = "";
   document.getElementById("answer-box").focus();
 
@@ -37,10 +38,12 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-  } else if (gameType === "subtract") {
-    displaySubtractQuestion(num1, num2);
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -80,10 +83,12 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
-  } else if (operator === "-") {
-    return [operand1 - operand2, "subtract"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -113,8 +118,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
   document.getElementById("operator").textContent = "-";
 }
 
@@ -124,8 +131,9 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "x";
 }
 
-//function displayDivideQuestion(operand1, operand2) {
-   // document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-   // document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-//document.getElementById("operator").textContent = "÷";
-  //}
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "/";
+}
